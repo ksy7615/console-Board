df07c0ed198427d70f0e9cb07a43f01c9098cad6
ksy7615 <sykim7615@daum.net>
2024-03-28 17:31:58 +0900

    Create Main Class

---
+++ console Board/src/consoleBoard/Main.java
@@ -0,0 +1,21 @@
+package consoleBoard;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ConsoleBoard board = new ConsoleBoard();
+		board.run();
+		
+		// 콘솔 게시판 만들기
+		// Map 활용
+		// User CRUD
+		// 기본적인 기능
+		// 로그인 / 로그아웃 / 회원가입 / 회원탈퇴
+
+		// Board CRUD
+		// 글 조회(전체) / 글 추가 / 글 수정(권한:글쓴이) / 글 삭제(권한:글쓴이)
+		// >> 글 작성자만 권한 O
+
+	}
+
+}
