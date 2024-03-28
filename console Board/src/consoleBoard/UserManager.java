de0d1c5663b5028a146312100c082cc67a3b634d
ksy7615 <sykim7615@daum.net>
2024-03-28 18:30:39 +0900

    Add isValidId Method & this method in addUser Of Class UserManager

--- console Board/src/consoleBoard/UserManager.java
+++ console Board/src/consoleBoard/UserManager.java
@@ -4,19 +4,30 @@ import java.util.ArrayList;
 
 public class UserManager {
 	ArrayList<User> list;
-	
+
 	public UserManager() {
 		list = new ArrayList<>();
 	}
-	
+
 	public User addUser(String id, String password) {
-		User user = new User(id, password);
-		list.add(user);
-		return user.clone();
+		if (isValidUser(id)) {
+			User user = new User(id, password);
+			list.add(user);
+			return user.clone();
+		}
+		return new User();
 	}
-	
+
 	public User getUser(int index) {
 		User user = list.get(index);
 		return user;
+	}
+
+	public boolean isValidUser(String id) {
+		for (User user : list) {
+			if (id.equals(user.getId()))
+				return false;
+		}
+		return true;
 	}
 }
