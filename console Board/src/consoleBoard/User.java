96f15c1db31796e457c16e755212109c6f41e0f8
ksy7615 <sykim7615@daum.net>
2024-03-28 18:28:35 +0900

    Add Default Constructor Of Class User

--- console Board/src/consoleBoard/User.java
+++ console Board/src/consoleBoard/User.java
@@ -4,6 +4,10 @@ public class User {
 	private String id, password;
 	private boolean isRight;
 	
+	public User() {
+		
+	}
+	
 	public User(String id, String password) {
 		this.id = id;
 		this.password = password;
