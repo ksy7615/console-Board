57dea44110beec8da56b105e629c5da7ea55c43f
ksy7615 <sykim7615@daum.net>
2024-03-28 17:59:49 +0900

    Create Getter&Setter + clone Of Class User

--- console Board/src/consoleBoard/User.java
+++ console Board/src/consoleBoard/User.java
@@ -1,5 +1,32 @@
 package consoleBoard;
 
 public class User {
+	private String id, password;
+	private boolean isRight;
+	
+	public User(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public boolean isRight() {
+		return this.isRight;
+	}
+
+	public void setRight() {
+		this.isRight = !isRight;
+	}
+	
+	public User clone() {
+		return new User(this.id, this.password);
+	}
 
 }
