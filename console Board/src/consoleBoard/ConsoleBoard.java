e151c3efa320e51df7ac6f327f4e0d3e7b945be1
ksy7615 <sykim7615@daum.net>
2024-03-29 10:49:22 +0900

    Add printBoard Of Class ConsoleBoard

--- console Board/src/consoleBoard/ConsoleBoard.java
+++ console Board/src/consoleBoard/ConsoleBoard.java
@@ -13,6 +13,20 @@ public class ConsoleBoard {
 		log = -1;
 	}
 	
+	private void printBoard() {
+		System.out.println("-----BOARD-----");
+		System.out.println("[1] 회원가입");
+		System.out.println("[2] 회원탈퇴");
+		System.out.println("[3] 로그인");
+		System.out.println("[4] 로그아웃");
+		System.out.println("[5] 게시글 조회");
+		System.out.println("[6] 게시글 작성");
+		System.out.println("[7] 게시글 수정");
+		System.out.println("[8] 게시글 삭제");
+		System.out.println("[0] 종료");
+		System.out.println("---------------");
+	}
+	
 	public void run() {
 		
 	}
