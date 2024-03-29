87861265dd66caf1294e028fd0a642533e74b2f9
ksy7615 <sykim7615@daum.net>
2024-03-29 15:07:11 +0900

    Create addPost Method Of Class BoardManager

--- console Board/src/consoleBoard/BoardManager.java
+++ console Board/src/consoleBoard/BoardManager.java
@@ -1,6 +1,24 @@
 package consoleBoard;
 
+import java.util.ArrayList;
+
 public class BoardManager {
+	private ArrayList<Post> board;
 	
+	public BoardManager() {
+		board = new ArrayList<>();
+	}
+	
+	public Post addPost(String title, String content, User user) {
+		// 로그인 되면 유저 활성화 되게 해놨으므로 null 아닐 때 포스트 작성
+		if (user != null) {
+			if (title != null && content != null) {
+				Post post = new Post(title, content);
+				board.add(post);
+				return post.clone();
+			}
+		}
+		return new Post();
+	}
 
 }
