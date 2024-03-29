d31453758d61cc4c501cb7146166c778e3a980c7
ksy7615 <sykim7615@daum.net>
2024-03-29 15:14:00 +0900

    Add getPost Of Class BoardManager

--- console Board/src/consoleBoard/BoardManager.java
+++ console Board/src/consoleBoard/BoardManager.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public class BoardManager {
 	private ArrayList<Post> board;
-	
+
 	public BoardManager() {
 		board = new ArrayList<>();
 	}
-	
+
 	public Post addPost(String title, String content, User user) {
 		// 로그인 되면 유저 활성화 되게 해놨으므로 null 아닐 때 포스트 작성
 		if (user != null) {
@@ -16,6 +16,16 @@ public class BoardManager {
 				Post post = new Post(title, content);
 				board.add(post);
 				return post.clone();
+			}
+		}
+		return new Post();
+	}
+
+	// 작성자가 쓴 글 보여주기
+	public Post getPost(User user) {
+		for(Post post : board) {
+			if(post.getWriterId().equals(user.getId())) {
+				return post;
 			}
 		}
 		return new Post();
