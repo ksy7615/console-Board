0afb078daf1d7d2f16010be1e97ba6c9b20f4f9e
ksy7615 <sykim7615@daum.net>
2024-03-29 10:45:29 +0900

    Create Constructor Of Class ConsoleBoard

--- console Board/src/consoleBoard/ConsoleBoard.java
+++ console Board/src/consoleBoard/ConsoleBoard.java
@@ -5,8 +5,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConsoleBoard {
-	private Map<User, ArrayList<Board>> map = new HashMap<>();
+	private Map<User, ArrayList<Board>> map;
 	private int log;
+	
+	public ConsoleBoard() {
+		map = new HashMap<>();
+		log = -1;
+	}
 	
 	public void run() {
 		
