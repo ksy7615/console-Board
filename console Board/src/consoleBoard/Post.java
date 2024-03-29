3a3e291d6eee7fa39448f852f9ca425b876672f5
ksy7615 <sykim7615@daum.net>
2024-03-29 14:52:44 +0900

    Modify class Name (Board > Post)

---
+++ console Board/src/consoleBoard/Post.java
@@ -0,0 +1,38 @@
+package consoleBoard;
+
+public class Post {
+	// Board CRUD
+	String title, content;
+	String writerId;
+
+	public Post() {
+
+	}
+
+	public Post(String title, String content, String writerId) {
+		this.title = title;
+		this.content = content;
+		this.writerId = writerId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Post clone() {
+		return new Post(this.title, this.content, this.writerId);
+	}
+
+}
