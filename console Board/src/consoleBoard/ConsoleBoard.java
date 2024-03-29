1bb7380ddf39f0ce4272b4dcc0c2d7920f4cd3bf
ksy7615 <sykim7615@daum.net>
2024-03-29 09:44:58 +0900

    Add Getter&Setter Of Class Board

--- console Board/src/consoleBoard/ConsoleBoard.java
+++ console Board/src/consoleBoard/ConsoleBoard.java
@@ -1,10 +1,14 @@
 package consoleBoard;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConsoleBoard {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Map<User, ArrayList<Board>> map = new HashMap<>();
+	
+	public void run() {
+		
 	}
 
 }
