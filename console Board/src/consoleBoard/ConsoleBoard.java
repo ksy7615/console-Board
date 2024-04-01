04ab6e6c4976867dfe179c11ce7252bd286a1e9c
ksy7615 <sykim7615@daum.net>
2024-04-01 11:34:23 +0900

    Modify viewAllPosts Of Class ConsoleBoard

--- console Board/src/consoleBoard/ConsoleBoard.java
+++ console Board/src/consoleBoard/ConsoleBoard.java
@@ -14,7 +14,7 @@ public class ConsoleBoard {
 	private UserManager userManager;
 	private BoardManager boardManager;
 	private User user;
-	
+
 	private AnonymousUser anonymousUser;
 	private boolean isExit;
 
@@ -25,7 +25,7 @@ public class ConsoleBoard {
 		board = new ArrayList<>();
 		userManager = UserManager.getInstance();
 		boardManager = BoardManager.getInstance();
-		
+
 		user = null;
 		anonymousUser = null;
 	}
@@ -59,18 +59,18 @@ public class ConsoleBoard {
 
 		System.out.println("회원가입 완료");
 	}
-	
+
 	private void leave() {
 		if (this.user != null) {
 			String password = inputString("password 재확인");
-			
-			if(userManager.isValidLogin(this.user.getId(), password)) {
+
+			if (userManager.isValidLogin(this.user.getId(), password)) {
 				userManager.removeUser(this.user);
 				boardManager.removeUserPostsAll(this.user);
-				
+
 				map.remove(this.user);
 				this.user = null;
-				
+
 				System.out.println("탈퇴 완료");
 			} else {
 				System.err.println("비밀번호가 일치하지 않습니다.");
@@ -103,7 +103,7 @@ public class ConsoleBoard {
 		this.user = null;
 		System.out.println("로그아웃 완료");
 	}
-	
+
 	private void printWritePostMenu() {
 		System.out.println("[1] 공개 글쓰기");
 		System.out.println("[2] 익명 글쓰기");
@@ -123,35 +123,34 @@ public class ConsoleBoard {
 		map.get(this.user).add(post);
 		System.out.println("포스팅이 등록되었습니다.");
 	}
-	
+
 	private void writePostAnonymous() {
 		if (this.user == null) {
 			System.err.println("회원만 포스팅할 수 있습니다.");
 			return;
 		}
 		// 모든 유저가 익명으로 글 쓸 권한이 존재
-		if(this.user instanceof Anonymous) {
+		if (this.user instanceof Anonymous) {
 			this.user.setAnonyMode();
-			
+
 			String title = inputString("제목");
 			String content = inputString("내용");
-			
-			if(this.user.anonyMode()) {
-				this.user.setId("ㅇㅇ");
-				
+
+			if (this.user.anonyMode()) {
 				Post post = new Post(title, content, user.getId());
 				board.add(post);
-				
+
 				map.get(this.user).add(post);
 			}
 			System.out.println("포스팅이 등록되었습니다.");
+			this.user.setAnonyMode();
 		}
 	}
-	
+
 	private void runWritePostMenu(int select) {
-		if(select == 1)
+		if (select == 1)
 			writePostPublic();
-		else if(select == 2)
+		else if (select == 2)
 			writePostAnonymous();
 	}
 
@@ -168,7 +167,11 @@ public class ConsoleBoard {
 		for (User user : map.keySet()) {
 			ArrayList<Post> userPosts = map.get(user);
 			for (int i = 0; i < userPosts.size(); i++) {
-				System.out.println(String.format("%d) 작성자 : %s\n%s", i + 1, user.getId(), userPosts.get(i)));
+				if (user.anonyMode()) {
+					System.out.println(String.format("%d) 작성자 : %s\n%s", i + 1, "*****", userPosts.get(i)));
+				} else {
+					System.out.println(String.format("%d) 작성자 : %s\n%s", i + 1, user.getId(), userPosts.get(i)));
+				}
 			}
 		}
 		System.out.println("===============");
@@ -235,7 +238,7 @@ public class ConsoleBoard {
 		else if (select == 2)
 			modifyContent();
 	}
-	
+
 	private void deletePost() {
 		if (this.user != null) {
 			viewMyPost();
@@ -250,7 +253,7 @@ public class ConsoleBoard {
 			boardManager.removePost(this.user, index);
 			map.get(this.user).remove(index);
 			board.remove(index);
-			
+
 			System.out.println("포스팅 삭제 완료");
 		} else {
 			System.err.println("로그인 후 이용 해주세요.");
@@ -261,7 +264,7 @@ public class ConsoleBoard {
 	private void runBoard(int select) {
 		if (select == 1)
 			join();
-		else if(select == 2)
+		else if (select == 2)
 			leave();
 		else if (select == 3)
 			login();
@@ -272,12 +275,10 @@ public class ConsoleBoard {
 		else if (select == 6) {
 			printWritePostMenu();
 			runWritePostMenu(inputNumber("선택"));
-		}
-		else if (select == 7) {
+		} else if (select == 7) {
 			printModifyPostMenu();
 			runModifyPostMenu(inputNumber("선택"));
-		}
-		else if (select == 8)
+		} else if (select == 8)
 			deletePost();
 		else if (select == 0)
 			isExit = true;
